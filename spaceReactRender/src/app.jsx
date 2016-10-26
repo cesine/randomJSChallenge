76453c5b858e1fea68ed8fd73a8b739cbb192084
MatMath <mathieu.k.legault@gmail.com>
2016-10-26 16:34:56 +0200

    Adding atom linting for es6 Fetch happen at loading.

--- spaceReactRender/src/app.jsx
+++ spaceReactRender/src/app.jsx
@@ -1,54 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './index.scss';
-import React from 'react';
-
-function getParamList(callback) {
-console.log("fetching param");
-  fetch('http://localhost:1701/param',{
-    method: 'GET',
-    ContentType: 'json'
-  })
-  .then(function(res) {
-    // This return the header call of the function, not the data.
-    return res.json();
-  })
-  .then(function(data){
-    // console.log('Got the ParamList', data);
-    if (data.persPerShip) {
-      callback(data);
-    }
-  })
-  .catch(function(ex) {
-    // Fail to fetch so keep using the default value.
-      window.console.log('parsing failed', ex);
-    })
-}
+import 'bootstrap/dist/css/bootstrap.min.css'
+import styles from './index.scss'
+import React from 'react'
 
 export default class App extends React.Component {
   constructor (props) {
-    super(props)
+    super(props);
     this.state = {
-      someValue: 12,
-      param: {}
-    }
-    this.paramlist = this.paramlist.bind(this);
+      someValue: 12
+    };
   }
-  paramlist() {
-    var updateThisState = function (data) {
-      this.setState({param: data});
-    }
-    updateThisState = updateThisState.bind(this);
-    getParamList(updateThisState);
-  }
-  render() {
-    const {someValue, param} = this.state;
+  render () {
+    const {someValue} = this.state;
+    const {persPerShip, engineMalfunction} = this.props.param;
     return (
       <div>
-        <h1>It Works!</h1>
         <p>This React project just works including <span className={styles.blueBg}>module</span> local styles.</p>
         <p>Global bootstrap css import works too as you can see on the following button.</p>
-        <p><a className="btn btn-primary btn-lg" onClick={this.paramlist} >Enjoy!</a></p>
-        <div>Param here: {someValue}, {param.persPerShip}</div>
+        <p><a className='btn btn-primary btn-lg' >Enjoy!</a></p>
+        <div>Param here: {someValue}, {persPerShip}, {engineMalfunction}</div>
       </div>
     )
   }
--- spaceReactRender/src/index.jsx
+++ spaceReactRender/src/index.jsx
@@ -1,5 +1,56 @@
+/*jshint esversion: 6 */
+
 import React from 'react';
 import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './app.jsx';
+import Errorpage from './errorpage';
 
-render(<App/>, document.querySelector("#app"));
+// fallback is the server dosent answer
+var fetchedParam = {
+  persPerShip: 100,
+  engineMalfunction: 0.01,
+  refuilingDefect: 0.02,
+  landingFaillure: 0.05,
+  reusabilityOfShip: 5,
+  improvement: 0.05,
+  firstStageEngine: 42,
+  itsEngine: 9,
+  touristRatio: 0.3,
+  orbitRefulling: 4,
+  probIncreaseProdOfIts: 0,
+  itsIncreaseOf: 1
+};
+const HTTP_SERVER = 'http://localhost:1701';
+
+
+var startTheApp = () => {
+
+  var getParamList = () => {
+    console.log('fetching param');
+    fetch(HTTP_SERVER+'/param',{
+      method: 'GET',
+      ContentType: 'json'
+    })
+    .then(function(res) {
+      // This return the header call of the function, not the data.
+      return res.json();
+    })
+    .then(function(data){
+      // console.log('Got the ParamList', data);
+      if (data.persPerShip) {
+        fetchedParam = data;
+      }
+      render(<App param={fetchedParam}/>, document.querySelector('#app'));
+    })
+    .catch(function(ex) {
+      // Fail to fetch so keep using the default value.
+        window.console.log('parsing failed', ex);
+        ReactDOM.render(<Errorpage />, document.getElementById('app'));
+      })
+  }
+
+
+  getParamList();
+}
+startTheApp();
