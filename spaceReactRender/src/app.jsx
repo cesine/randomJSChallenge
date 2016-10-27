469842a926925dd32a82d230fd70014305f4eda0
MatMath <mathieu.k.legault@gmail.com>
2016-10-27 12:42:45 +0200

    Working structure with "2way" data binding. (refresh with input).

--- spaceReactRender/src/app.jsx
+++ spaceReactRender/src/app.jsx
@@ -6,18 +6,71 @@ export default class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      someValue: 12
+      persPerShip: this.props.param.persPerShip,
+      engineMalfunction: this.props.param.engineMalfunction,
+      refuilingDefect: this.props.param.refuilingDefect,
+      landingFaillure: this.props.param.landingFaillure,
+      reusabilityOfShip: this.props.param.reusabilityOfShip,
+      improvement: this.props.param.improvement,
+      firstStageEngine: this.props.param.firstStageEngine,
+      itsEngine: this.props.param.itsEngine,
+      touristRatio: this.props.param.touristRatio,
+      orbitRefulling: this.props.param.orbitRefulling,
+      probIncreaseProdOfIts: this.props.param.probIncreaseProdOfIts,
+      itsIncreaseOf: this.props.param.itsIncreaseOf
     };
+    this.changeNumberValue = this.changeNumberValue.bind(this);
+  }
+  changeNumberValue (key, event) {
+    // This set the value of the proper Key. Note, Key is the second argument after "this"!?! and event is something else that arrive magically.
+    // console.log('key:', key);
+    // console.log('event:', event, event.target.value);
+    this.setState({[key]: parseFloat(event.target.value)});
   }
   render () {
-    const {someValue} = this.state;
-    const {persPerShip, engineMalfunction} = this.props.param;
+    var {persPerShip, engineMalfunction} = this.state;
     return (
-      <div>
-        <p>This React project just works including <span className={styles.blueBg}>module</span> local styles.</p>
-        <p>Global bootstrap css import works too as you can see on the following button.</p>
+      <div className='container'>
+        <div>Param here: {persPerShip}, {engineMalfunction}</div>
+        <div>  persPerShip: {persPerShip}</div>
+        <div className='row'>
+
+          <div className='col-sm-6'>
+            <div className="form-group">
+              <div className="input-group">
+                <div className="input-group-addon"># person per ship</div>
+                <input type="number" className="form-control"
+                  value={this.state.persPerShip}
+                  onChange={this.changeNumberValue.bind(this, 'persPerShip')} />
+              </div>
+            </div>
+          </div>
+
+          <div className='col-sm-6'>
+            <div className="form-group">
+              <div className="input-group">
+                <div className="input-group-addon"># person per ship</div>
+                <input type="number" className="form-control"
+                  value={this.state.engineMalfunction}
+                  onChange={this.changeNumberValue.bind(this, 'engineMalfunction')} />
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+          engineMalfunction: 0.01,
+          refuilingDefect: 0.02,
+          landingFaillure: 0.05,
+          reusabilityOfShip: 5,
+          improvement: 0.05,
+          firstStageEngine: 42,
+          itsEngine: 9,
+          touristRatio: 0.3,
+          orbitRefulling: 4,
+          probIncreaseProdOfIts: 0,
+          itsIncreaseOf: 1,
         <p><a className='btn btn-primary btn-lg' >Enjoy!</a></p>
-        <div>Param here: {someValue}, {persPerShip}, {engineMalfunction}</div>
       </div>
     )
   }
