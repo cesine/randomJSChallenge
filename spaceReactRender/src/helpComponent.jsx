1e01365c064d1e913a95a4e7c25a7bc271c44627
MatMath <mathieu.k.legault@gmail.com>
2016-11-02 16:19:49 +0100

    Adding show-hide logic inside the display component.

--- spaceReactRender/src/helpComponent.jsx
+++ spaceReactRender/src/helpComponent.jsx
@@ -5,17 +5,34 @@ export default class HelpComponentList extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      msgArray: this.props.msgArray
+      msgArray: this.props.msgArray,
+      display: true
     }
+    this.setToTrue = this.setToTrue.bind(this);
+    this.setToFalse = this.setToFalse.bind(this);
+  }
+  setToTrue() {
+    this.setState({display: true});
+    console.log("Set to true");
+  }
+  setToFalse() {
+    console.log("Set to False");
+    this.setState({display: false});
   }
   render() {
-    const {msgArray} = this.state;
+    const {msgArray, display} = this.state;
     return (
-      <ul>
-        {msgArray.map((string, index) => {
-          return (<li key={index}>{string}</li>)
-        })}
-      </ul>
+      <div>
+        {display ? <button className='btn btn-info' onClick={this.setToFalse}>Hide</button> : null}
+        {!display ? <button className='btn btn-info' onClick={this.setToTrue}>Info</button> : null}
+        <span>{display}</span>
+        {display ?
+          <ul>
+          {msgArray.map((string, index) => {
+            return (<li key={index}>{string}</li>)
+          })}
+          </ul> : null }
+      </div>
     )
   }
 }
