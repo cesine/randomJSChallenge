3bc72df5e3c1e700e57f8859fe86d635438b94ea
MatMath <mathieu.k.legault@gmail.com>
2016-11-05 21:56:35 +0100

    Doing a manual graph to start.

--- spaceReactRender/src/graphSection.jsx
+++ spaceReactRender/src/graphSection.jsx
@@ -11,10 +11,32 @@ export default class GraphSection extends React.Component {
     }
   }
   render() {
-    const {msgArray, display} = this.state;
+    const {display, resultOfgrowth} = this.state;
+    let maximumWidthRatio = 500/resultOfgrowth[resultOfgrowth.length - 1].martian;
+    // 500 is a arbritary number of the maximum nbr of pixel width the table is allowed to take. 
     return (
       <div>
         <span>{display}</span>
+        Graph that could be fun:
+        <ul>
+          <li>Population Growth - dot line</li>
+          <li>Total death</li>
+          <li>Nbr ship lost over time</li>
+          <li>Pie Shart / bar of type of defect over time.</li>
+          <li>Comparing Current run vs previous run</li>
+        </ul>
+
+        <h3>Population Growth</h3>
+        {resultOfgrowth.map((item, index)=> {
+          return (
+            <div className="row" key={index}>
+              <div className="col-xs-2">{item.martian}</div>
+              <div className="col-xs-3" style={{width:item.martian*maximumWidthRatio,'backgroundColor': 'blue'}}>{item.martian}</div>
+          </div>);
+        })}
+        <div>
+
+        </div>
       </div>
     )
   }
