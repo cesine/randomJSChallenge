16a9a12034ed2513e3064e876dd6273bc241ce77
MatMath <mathieu.k.legault@gmail.com>
2016-11-06 23:12:31 +0100

    hiding the graph at loading

--- spaceReactRender/src/graphSection.jsx
+++ spaceReactRender/src/graphSection.jsx
@@ -9,11 +9,16 @@ export default class GraphSection extends React.Component {
     this.state = {
       resultOfgrowth: this.props.resultOfgrowth,
       savedBackup: this.props.savedBackup,
-      display: 'Yes'
+      display: 'Yes',
+      displayGraph: ''
     }
+    this.growthVsDeath = this.growthVsDeath.bind(this);
+  }
+  growthVsDeath() {
+    this.setState({displayGraph:'growthVsDeath'});
   }
   render() {
-    const {display, resultOfgrowth} = this.state;
+    const {display, resultOfgrowth, displayGraph} = this.state;
     let maximumWidthRatio = 500/resultOfgrowth[resultOfgrowth.length - 1].martian;
     // 500 is a arbritary number of the maximum nbr of pixel width the table is allowed to take.
     return (
@@ -21,7 +26,7 @@ export default class GraphSection extends React.Component {
         <span>{display}</span>
         Graph that could be fun:
         <ul>
-          <li>Population Growth vs Death occurence</li>
+          <li onClick={this.growthVsDeath}>Population Growth vs Death occurence</li>
           <li>Total death</li>
           <li>Nbr ship lost over time</li>
           <li>Pie Shart / bar of type of defect over time.</li>
@@ -29,7 +34,7 @@ export default class GraphSection extends React.Component {
         </ul>
 
         <h3>Population Growth</h3>
-        {resultOfgrowth.map((item, index)=> {
+        {displayGraph == 'growthVsDeath' && resultOfgrowth.map((item, index)=> {
           return (<div key={index}>
               <GraphBar nbr={item.martian} max={maximumWidthRatio} color='rgba(0, 0, 255, 0.7)' textColor='white'></GraphBar>
               <GraphBar nbr={item.cummulativeLife} max={maximumWidthRatio} color='#ec581f' textColor='black'></GraphBar>
