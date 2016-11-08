8c6da599d00ce2e0e39cafc6b2f56698d023a646
MatMath <mathieu.k.legault@gmail.com>
2016-11-08 11:43:47 +0100

    Adding the SVC version of the Pie Chart

--- spaceReactRender/src/graph/pieChart.jsx
+++ spaceReactRender/src/graph/pieChart.jsx
@@ -15,6 +15,9 @@ export default class PieChart extends React.Component {
         <div className={styles.pie} style={{animationDelay: '-20s'}}></div>
         <div className={styles.pie} style={{animationDelay: '-60s'}}></div>
         <div className={styles.pie} style={{animationDelay: '-90s'}}></div>
+        <svg viewBox="0 0 32 32">
+          <circle r="16" cx="16" cy="16" />
+        </svg>
       </div>)
   }
 }
