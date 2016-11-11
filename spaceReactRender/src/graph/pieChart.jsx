d2145e4428051888e5cf2f5f323b19ab1d9c6ac6
MatMath <mathieu.k.legault@gmail.com>
2016-11-11 18:37:53 +0100

    Connecting the Graph Selector to the Pie chart

--- spaceReactRender/src/graph/pieChart.jsx
+++ spaceReactRender/src/graph/pieChart.jsx
@@ -5,23 +5,20 @@ import styles from './pieGraph.scss'
 var rawData = [
     {
         percentage: 0.45,
-        label: 'Thing 1',
-        color: '#61C0BF'
+        label: 'Thing 1'
+
     },
     {
         percentage: 0.21,
-        label: "Thing Two",
-        color: '#DA507A'
+        label: "Thing Two"
     },
     {
         percentage: 0.11,
-        label: "Another Thing",
-        color: '#BB3D49'
+        label: "Another Thing"
     },
     {
         percentage: 0.23,
-        label: "Pineapple",
-        color: '#DB4547'
+        label: "Pineapple"
     }
 ];
 // Started inpiration from: https://www.smashingmagazine.com/2015/07/designing-simple-pie-charts-with-css/
@@ -32,13 +29,13 @@ var rawData = [
       this.state = {
         size: 230,
         radius: 115,
-        sectors: this.calculateSectors(rawData, 230),
+        sectors: this.calculateSectors(this.props.deathRatio, 230)
     }
     this.calculateSectors = this.calculateSectors.bind(this);
   }
   calculateSectors( data, size ) {
     var sectors = [];
-
+    var colors = ["#61C0BF", "#DA507A", "#BB3D49", "#DB4547", "#178ad6", "#17d6c7", "#17d66f"]
     var l = size / 2;
     var a = 0; // Angle
     var aRad = 0; // Angle in Rad
@@ -77,7 +74,7 @@ var rawData = [
         sectors.push({
             percentage: item.percentage,
             label: item.label,
-            color: item.color,
+            color: colors[key],
             arcSweep: arcSweep,
             L: l,
             X: X,
--- spaceReactRender/src/graphSection.jsx
+++ spaceReactRender/src/graphSection.jsx
@@ -11,28 +11,54 @@ export default class GraphSection extends React.Component {
     this.state = {
       resultOfgrowth: this.props.resultOfgrowth,
       savedBackup: this.props.savedBackup,
+      deathRatio: this.deathRatio(this.props.resultOfgrowth),
       display: 'Yes',
       displayGraph: ''
     }
     this.growthVsDeath = this.growthVsDeath.bind(this);
+    this.pieChart = this.pieChart.bind(this);
   }
   growthVsDeath() {
     this.setState({displayGraph:'growthVsDeath'});
   }
+  pieChart() {
+    this.setState({displayGraph:'pieChart'});
+  }
+  deathRatio(item) {
+    return [
+        {
+            percentage: 0.45,
+            label: 'Thing 1'
+
+        },
+        {
+            percentage: 0.21,
+            label: "Thing Two"
+        },
+        {
+            percentage: 0.11,
+            label: "Another Thing"
+        },
+        {
+            percentage: 0.23,
+            label: "Pineapple"
+        }
+    ];
+  }
   render() {
-    const {display, resultOfgrowth, displayGraph, savedBackup} = this.state;
+    const {display, resultOfgrowth, displayGraph, savedBackup, deathRatio} = this.state;
     return (
       <div>
         Graph that could be fun:
         <ul>
           <li onClick={this.growthVsDeath}>Population Growth vs Death occurence</li>
-          <li>Nbr ship lost over time</li>
+          <li onClick={this.pieChart}>Nbr ship lost over time</li>
           <li>Pie Shart / bar of type of defect over time.</li>
           <li>Comparing Current run vs previous run</li>
         </ul>
 
         {displayGraph == 'growthVsDeath' && <BarGraph resultOfgrowth={resultOfgrowth} savedBackup={savedBackup}></BarGraph>}
-        <PieChart></PieChart>
+        {displayGraph == 'pieChart' && <PieChart deathRatio={deathRatio}></PieChart>}
       </div>
     )
   }
