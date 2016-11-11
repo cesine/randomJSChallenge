c84bad84418eb02ee0ab000830ae28350826b76b
MatMath <mathieu.k.legault@gmail.com>
2016-11-11 17:55:42 +0100

    Making the SVG Pie chart working.

--- spaceReactRender/src/graph/pieChart.jsx
+++ spaceReactRender/src/graph/pieChart.jsx
@@ -2,21 +2,103 @@ import React from 'react'
 
 import styles from './pieGraph.scss'
 
-export default class PieChart extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      nbr: 5
+var rawData = [
+    {
+        percentage: 0.45,
+        label: 'Thing 1',
+        color: '#61C0BF'
+    },
+    {
+        percentage: 0.21,
+        label: "Thing Two",
+        color: '#DA507A'
+    },
+    {
+        percentage: 0.11,
+        label: "Another Thing",
+        color: '#BB3D49'
+    },
+    {
+        percentage: 0.23,
+        label: "Pineapple",
+        color: '#DB4547'
     }
+];
+// Started inpiration from: https://www.smashingmagazine.com/2015/07/designing-simple-pie-charts-with-css/
+// But Selected in SVG: https://danielpataki.com/svg-pie-chart-javascript/
+  export default class PieChart extends React.Component {
+    constructor (props) {
+      super(props);
+      this.state = {
+        size: 230,
+        radius: 115,
+        sectors: this.calculateSectors(rawData, 230),
+    }
+    this.calculateSectors = this.calculateSectors.bind(this);
+  }
+  calculateSectors( data, size ) {
+    var sectors = [];
+
+    var l = size / 2;
+    var a = 0; // Angle
+    var aRad = 0; // Angle in Rad
+    var z = 0; // Size z
+    var x = 0; // Side x
+    var y = 0; // Side y
+    var X = 0; // SVG X coordinate
+    var Y = 0; // SVG Y coordinate
+    var R = 0; // Rotation
+    var arcSweep = 0;
+
+    data.map( function(item, key ) {
+        a = 360 * item.percentage;
+        var aCalc = ( a > 180 ) ? 360 - a : a;
+        aRad = aCalc * Math.PI / 180;
+        z = Math.sqrt( 2*l*l - ( 2*l*l*Math.cos(aRad) ) );
+        if( aCalc <= 90 ) {
+            x = l*Math.sin(aRad);
+        }
+        else {
+            x = l*Math.sin((180 - aCalc) * Math.PI/180 );
+        }
+
+        y = Math.sqrt( z*z - x*x );
+        Y = y;
+
+        if( a <= 180 ) {
+            X = l + x;
+            arcSweep = 0;
+        }
+        else {
+            X = l - x;
+            arcSweep = 1;
+        }
+
+        sectors.push({
+            percentage: item.percentage,
+            label: item.label,
+            color: item.color,
+            arcSweep: arcSweep,
+            L: l,
+            X: X,
+            Y: Y,
+            R: R
+        });
+
+        R = R + a;
+    })
+
+
+    return sectors
   }
   render() {
+    const {size, sectors, radius} = this.state;
     return (<div>
       <h1>PIE CHART</h1>
-        <div className={styles.pie} style={{animationDelay: '-20s'}}></div>
-        <div className={styles.pie} style={{animationDelay: '-60s'}}></div>
-        <div className={styles.pie} style={{animationDelay: '-90s'}}></div>
-        <svg viewBox="0 0 32 32">
-          <circle r="16" cx="16" cy="16" />
+        <svg style={{width: 230+'px', 'height': 230+'px'}}>
+          {sectors.map((item, index) => {
+            return (<path key={index} fill={item.color} d={`M${radius},${radius} L${radius},0 A${radius},${radius} 1 0,1 ${item.X}, ${item.Y} z`} transform={`rotate(${item.R}, ${radius}, ${radius})`}></path>)
+          })}
         </svg>
       </div>)
   }
