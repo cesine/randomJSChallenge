946111d582d5db6dd4eb94983ce2c4e3ad9c57e6
cesine <cesine@yahoo.com>
2019-03-23 16:26:34 -0400

    example solution
    https://khan4019.github.io/front-end-Interview-Questions/sort.html

--- algorithm/sort-spec.js
+++ algorithm/sort-spec.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const expect = require('expect.js');
 
-const {native, quicksort} = require('./sort');
+const { native, quicksort, partition, swap } = require('./sort');
 
 describe('sort', () => {
   function compareNumbers(a, b) {
@@ -64,6 +64,50 @@ describe('sort', () => {
   });
 
   describe('quicksort', () => {
+    it('should swap', () => {
+      expect(swap(['x', 'xx', 'xxx'], 0, 2)).to.eql(['xxx', 'xx', 'x']);
+    });
+
+    it('should partition with no change', () => {
+      const input = [1, 2, 3];
+      expect(partition(input, 2, 0, 2)).to.equal(2);
+      expect(input).to.eql([1, 2, 3]);
+    });
+
+    it('should partition with inverse order', () => {
+      const input = [3, 2, 1];
+      expect(partition(input, 2, 0, 2)).to.equal(0);
+      expect(input).to.eql([1, 2, 3]);
+    });
+
+    it('should partition with random order', () => {
+      const input = [3, 7, 8, 4, 2, 1, 5];
+      expect(partition(input, 6, 0, 6)).to.equal(4);
+      expect(input).to.eql([3, 4, 2, 1, 5, 7, 8]);
+    });
+
+    it('should partition with another random order', () => {
+      const input = [3, 8, 7, 4, 2, 1, 5];
+      expect(partition(input, 6, 0, 6)).to.equal(4);
+      expect(input).to.eql([3, 4, 2, 1, 5, 8, 7]);
+    });
+
+    it('should partition inside an array', () => {
+      const input = [3, 4, 2, 1, 5, 8, 7];
+      expect(partition(input, 3, 0, 3)).to.equal(0);
+      expect(input).to.eql([1, 4, 2, 3, 5, 8, 7]);
+    });
+
+    it('should partition inside an array again', () => {
+      const input = [1, 4, 2, 3, 5, 8, 7];
+      expect(partition(input, 3, 0, 3)).to.equal(2);
+      expect(input).to.eql([1, 2, 3, 4, 5, 8, 7]);
+    });
+
+    it('should sort random numbers', () => {
+      expect(quicksort([3, 8, 7, 4, 2, 1, 5], compareNumbers)).to.eql([1, 2, 3, 4, 5, 7, 8]);
+    });
+
     it('should sort numbers', () => {
       expect(quicksort([1, 7, 3, 4, 2, 6, 5, 8, 9], compareNumbers)).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
     });
--- algorithm/sort.js
+++ algorithm/sort.js
@@ -1,5 +1,5 @@
 /**
- * Sort an array of inputs using the native sort function
+ * Sort an input of inputs using the native sort function
  * @param input
  * @returns {Array}
  */
@@ -7,68 +7,54 @@ function native(input, compare) {
   return input.sort(compare);
 }
 
+function swap(input, i, j) {
+  const temp = input[i];
+  // eslint-disable-next-line no-param-reassign
+  input[i] = input[j];
+  // eslint-disable-next-line no-param-reassign
+  input[j] = temp;
+  return input;
+}
+
+function partition(input, pivot, left, right) {
+  let partitionIndex = left;
+  // loops through the array from left to right
+  for (let i = left; i < right; i++) {
+    // if the value at left is too big, move it over one
+    if (input[i] < input[pivot]) {
+      swap(input, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+
+  swap(input, partitionIndex, pivot);
+  return partitionIndex;
+}
+
+function qSort(input, left, right) {
+  if (left < right) {
+    const pivot = right;
+    const partitionIndex = partition(input, pivot, left, right);
+
+    qSort(input, left, partitionIndex - 1);
+    qSort(input, partitionIndex + 1, right);
+  }
+
+  return input;
+}
+
 /**
- * Sort an array of inputs using the quicksort algorithm
+ * Sort an input of inputs using the quicksort algorithm
  * @param input
  * @returns {Array}
  */
 function quicksort(input, compare) {
-  const output = input;
-
-  // Recursively sort sub-arrays.
-  const recursiveSort = (start, end) => {
-    // If this sub-array contains less than 2 elements, it's sorted.
-    if (end - start < 1) {
-      return;
-    }
-    console.log(` recursiveSort ${start} ${end}`);
-
-    const pivotValue = output[end];
-    let splitIndex = start;
-    for (let i = start; i < end; i++) {
-      console.log(`  ${splitIndex}:${output[splitIndex]} vs ${i}:${output[i]}`)
-      if (compare(output[i], pivotValue) > 0) {
-        // No swap needed
-        // console.log(`    no swap ${output}`);
-        continue;
-      }
-
-      // If the element just to the right of the split index,
-      //   isn't this element, swap them.
-      if (splitIndex !== i) {
-        const temp = output[splitIndex];
-        console.log(`    swap ${output} ${splitIndex}:${temp} and ${i}:${output[i]}`);
-        output[splitIndex] = output[i];
-        output[i] = temp;
-        console.log(`      swap ${output}`);
-      }
-
-      // Move the split index to the right by one, denoting an increase in the less-than sub-array size.
-      splitIndex++;
-    }
-
-    // Move the pivot value to between the split.
-    if (output[end] !== output[splitIndex]) {
-      console.log(`    swap pivot ${output} (${splitIndex}:${output[splitIndex]} and ${end}:${output[end]})`);
-      output[end] = output[splitIndex];
-      output[splitIndex] = pivotValue;
-      console.log(`      swap pivot ${output}`);
-    }
-
-    // Recursively sort the less-than and greater-than arrays.
-    console.log(`Sort right ${splitIndex}`);
-    recursiveSort(start, splitIndex - 1);
-    console.log(`Sort left ${splitIndex}`);
-    recursiveSort(splitIndex + 1, end);
-  };
-
-  // Sort the entire array.
-  console.log('Sort all', input);
-  recursiveSort(0, input.length - 1);
-  return output;
+  return qSort(input, 0, input.length - 1);
 };
 
 module.exports = {
   native,
   quicksort,
+  partition,
+  swap,
 };
