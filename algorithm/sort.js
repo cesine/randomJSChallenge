e50ea65abd70f918a27191d88eca18324a831e5c
cesine <cesine@yahoo.com>
2019-03-23 14:44:22 -0400

    example solution
    https://medium.com/@Charles_Stover/implementing-quicksort-in-javascript-8044a8e2bf39

--- algorithm/sort.js
+++ algorithm/sort.js
@@ -13,34 +13,60 @@ function native(input, compare) {
  * @returns {Array}
  */
 function quicksort(input, compare) {
-  if (input.length <= 1) {
-    return input;
-  }
+  const output = input;
 
-  // median of three
-  const pivot = Math.ceil(Math.max(
-    input[0],
-    input[input.length - 1],
-    input[Math.ceil(input.length - 1 / 2)],
-  ) / 2 );
-
-  for (let i = 0; i < input.length / 2; i++) {
-    const lastIndex = input.length - i - 1;
-    const a = input[i];
-    const b = input[lastIndex];
-    if (compare(a, pivot) > 0) {
-      input[i] = b;
-      input[lastIndex] = a;
+  // Recursively sort sub-arrays.
+  const recursiveSort = (start, end) => {
+    // If this sub-array contains less than 2 elements, it's sorted.
+    if (end - start < 1) {
+      return;
     }
-  }
+    console.log(` recursiveSort ${start} ${end}`);
 
+    const pivotValue = output[end];
+    let splitIndex = start;
+    for (let i = start; i < end; i++) {
+      console.log(`  ${splitIndex}:${output[splitIndex]} vs ${i}:${output[i]}`)
+      if (compare(output[i], pivotValue) > 0) {
+        // No swap needed
+        // console.log(`    no swap ${output}`);
+        continue;
+      }
 
-  const mid = Math.ceil(input.length / 2);
-  const first = input.slice(0, mid);
-  const second = input.slice(mid, input.length);
+      // If the element just to the right of the split index,
+      //   isn't this element, swap them.
+      if (splitIndex !== i) {
+        const temp = output[splitIndex];
+        console.log(`    swap ${output} ${splitIndex}:${temp} and ${i}:${output[i]}`);
+        output[splitIndex] = output[i];
+        output[i] = temp;
+        console.log(`      swap ${output}`);
+      }
 
-  return quicksort(first, compare).concat(quicksort(second, compare));
-}
+      // Move the split index to the right by one, denoting an increase in the less-than sub-array size.
+      splitIndex++;
+    }
+
+    // Move the pivot value to between the split.
+    if (output[end] !== output[splitIndex]) {
+      console.log(`    swap pivot ${output} (${splitIndex}:${output[splitIndex]} and ${end}:${output[end]})`);
+      output[end] = output[splitIndex];
+      output[splitIndex] = pivotValue;
+      console.log(`      swap pivot ${output}`);
+    }
+
+    // Recursively sort the less-than and greater-than arrays.
+    console.log(`Sort right ${splitIndex}`);
+    recursiveSort(start, splitIndex - 1);
+    console.log(`Sort left ${splitIndex}`);
+    recursiveSort(splitIndex + 1, end);
+  };
+
+  // Sort the entire array.
+  console.log('Sort all', input);
+  recursiveSort(0, input.length - 1);
+  return output;
+};
 
 module.exports = {
   native,
