26570ae3fd2ecda2a7720fead44c2ef347784ad1
cesine <cesine@yahoo.com>
2019-09-14 17:48:30 -0400

    enumerate permuations

--- algorithm/combinatorics-spec.js
+++ algorithm/combinatorics-spec.js
@@ -17,8 +17,37 @@ function factorial(n) {
   return memo[n];
 }
 
+function enumeratePermutations({ set, choose }) {
+  if (!set.length) {
+    return [];
+  }
 
-describe.only('combinatorics', () => {
+  const result = []
+  set.forEach((item) => {
+    console.log('item', item);
+    for (let i = 0; i < choose; i++) {
+      return set.map((innerItem) => {
+        console.log(`${i}th innerItem`, innerItem);
+        result.push([item, innerItem]);
+      });
+    };
+  });
+
+  // for (let i = 0; i < choose; i++) {
+  //   const items = [...set];
+  //   result[i] = [];
+
+  //   for (let j = items.length; j > 0; j--) {
+  //     item = items.pop();
+  //     console.log(`position ${i} item ${item}`);
+  //     result[i].push([set[i], item]);
+  //   }
+  // }
+
+  return Object.values(result).sort(Intl.Collator().compare);
+}
+
+describe('combinatorics', () => {
   describe('factorial', () => {
     it('should calculate factorial of zero', () => {
       expect(factorial(0)).to.equal(1);
@@ -39,6 +68,44 @@ describe.only('combinatorics', () => {
 
     it('should calculate factorials that were already calculated', () => {
       expect(factorial(12)).to.equal(479001600);
+    });
+  });
+
+  describe.only('enumeratePermutations', () => {
+    it('should enumerate the permutations of an empty set', () => {
+      expect(enumeratePermutations({ set: [] })).to.eql([]);
+    });
+
+    it('should enumerate the permutations of an small set', () => {
+      expect(enumeratePermutations({
+        set: ['A', 'K', 'B'],
+        choose: 1,
+      })).to.eql(['A', 'B', 'K']);
+    });
+
+    it('should enumerate the permutations of an small set', () => {
+      expect(enumeratePermutations({
+        set: ['A', 'B', 'C'],
+        choose: 2,
+      })).to.eql([
+        ['A', 'A'],
+        ['A', 'B'],
+        ['A', 'C'],
+        ['B', 'A'],
+        ['B', 'B'],
+        ['B', 'C'],
+        ['C', 'A'],
+        ['C', 'B'],
+        ['C', 'C'],
+      ]);
+    });
+
+    it.only('should enumerate the permutations of an small set', () => {
+      expect(enumeratePermutations({
+        set: ['A', 'B', 'C'],
+        choose: 3,
+      })).to.eql([
+      ]);
     });
   });
 });
